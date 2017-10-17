cf66fe45648f31ec5da2b99039a3660409dddfdc
lost273 <dnk273@gmail.com>
2017-10-17 13:19:38 +0300

    17-10-2017-13-19

--- Filters/Controllers/HomeController.cs
+++ Filters/Controllers/HomeController.cs
@@ -12,5 +12,14 @@ namespace Filters.Controllers
         public string Index() {
             return "This is the Index action on the Home controller";
         }
+        [HandleError(ExceptionType = typeof(ArgumentOutOfRangeException), View = "RangeError")]
+        public string RangeTest (int id) {
+            if (id > 100) {
+                return String.Format("The id value is:{0}", id);
+            }
+            else {
+                throw new ArgumentOutOfRangeException("id");
+            }
+        }
     }
 }
